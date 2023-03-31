de18c05d2b2972e4164c4109e500df2c573a33b0
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2023-03-31 06:00:21 +0000

    fix typo

--- schema/scripts/sql_template/setup_schema.sql
+++ schema/scripts/sql_template/setup_schema.sql
@@ -3,11 +3,11 @@ CREATE TABLE SLIPRATE_tb (
 
    SlipRateID  VARCHAR(10) UNIQUE NOT NULL,
    X           float DEFAULT 0.0,  
-   Y           folat DEFAULT 0.0,
+   Y           float DEFAULT 0.0,
    FaultName   VARCHAR(100) NOT NULL,
    SiteName    VARCHAR(100) NOT NULL,
    LowRate     float default 0.0,
-   HighRate    foat default 0.0,
+   HighRate    float default 0.0,
    State       VARCHAR(6) NOT NULL,
    DataType    VARCHAR(100) NOT NULL,
    QbinMin     float default 0.0,
