4b9d1966424eede96259d3c9c3a6ae1b1134324d
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2023-04-24 22:33:27 +0000

    fix typo

--- schema/sql/sliprate1/setup_schema.sql
+++ schema/sql/sliprate1/setup_schema.sql
@@ -14,13 +14,13 @@ CREATE TABLE SLIPRATE_tb (
    PrefRate      VARCHAR(50),
    LowRate       float default 0.0,
    HighRate      float default 0.0,
-   RateUnct      VARCHAR(200) NOT NULL
-   RateType      VARCHAR(100) NOT NULL
-   ReptReint     VARCHAR(200) NOT NULL
-   OffType       VARCHAR(200) NOT NULL
-   AgeType       VARCHAR(100) NOT NULL
-   NumEvents     VARCHAR(100) NOT NULL
-   RateAge       VARCHAR(100) NOT NULL
+   RateUnct      VARCHAR(200) NOT NULL,
+   RateType      VARCHAR(100) NOT NULL,
+   ReptReint     VARCHAR(200) NOT NULL,
+   OffType       VARCHAR(200) NOT NULL,
+   AgeType       VARCHAR(100) NOT NULL,
+   NumEvents     VARCHAR(100) NOT NULL,
+   RateAge       VARCHAR(100) NOT NULL,
    QbinMin       float default 0.0,
    QbinMax       float default 0.0,
    Reference     VARCHAR(200) NOT NULL,
