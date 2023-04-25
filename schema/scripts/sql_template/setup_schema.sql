589e3282bbe62f79055e7948a6d1b0a44517a737
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2023-04-25 04:08:06 +0000

    test with bigger VARCHAR

--- schema/scripts/sql_template/setup_schema.sql
+++ schema/scripts/sql_template/setup_schema.sql
@@ -14,16 +14,16 @@ CREATE TABLE SLIPRATE_tb (
    PrefRate      VARCHAR(50),
    LowRate       float default 0.0,
    HighRate      float default 0.0,
-   RateUnct      VARCHAR(200) NOT NULL,
-   RateType      VARCHAR(100) NOT NULL,
-   ReptReint     VARCHAR(200) NOT NULL,
-   OffType       VARCHAR(300) NOT NULL,
-   AgeType       VARCHAR(100) NOT NULL,
-   NumEvents     VARCHAR(100) NOT NULL,
+   RateUnct      VARCHAR(400) NOT NULL,
+   RateType      VARCHAR(400) NOT NULL,
+   ReptReint     VARCHAR(400) NOT NULL,
+   OffType       VARCHAR(400) NOT NULL,
+   AgeType       VARCHAR(400) NOT NULL,
+   NumEvents     VARCHAR(400) NOT NULL,
    RateAge       VARCHAR(100) NOT NULL,
    QbinMin       float default 0.0,
    QbinMax       float default 0.0,
-   Reference     VARCHAR(200) NOT NULL,
+   Reference     VARCHAR(400) NOT NULL,
    AppB          VARCHAR(400) NOT NULL
 );
 
