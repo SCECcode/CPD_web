f10ee287b7083bf60c5e6bdd50f1dedfad97929a
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2023-04-25 05:31:58 +0000

    wip

--- schema/sql/sliprate1/setup_schema.sql
+++ schema/sql/sliprate1/setup_schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE SLIPRATE_tb (
    gid           serial PRIMARY KEY,
    SliprateID    VARCHAR(4) UNIQUE NOT NULL,
+   CPDID         VARCHAR(4) UNIQUE NOT NULL,
    Longitude     float DEFAULT 0.0,  
    Latitude      float DEFAULT 0.0,  
    FaultName     VARCHAR(200) NOT NULL,
@@ -17,14 +18,13 @@ CREATE TABLE SLIPRATE_tb (
    RateUnct      VARCHAR(400) NOT NULL,
    RateType      VARCHAR(400) NOT NULL,
    ReptReint     VARCHAR(400) NOT NULL,
-   OffType       VARCHAR(400) NOT NULL,
+   OffsetType    VARCHAR(400) NOT NULL,
    AgeType       VARCHAR(400) NOT NULL,
    NumEvents     VARCHAR(400) NOT NULL,
    RateAge       VARCHAR(100) NOT NULL,
    QbinMin       float default 0.0,
    QbinMax       float default 0.0,
    Reference     VARCHAR(400) NOT NULL,
-   AppB          VARCHAR(400) NOT NULL
 );
 
 SELECT AddGeometryColumn('','sliprate_tb','geom','0','POINT',2);
