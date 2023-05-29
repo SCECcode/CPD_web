4c7cd02ad680ce6cf6b8f7253f641c51ccc25aa3
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2023-05-29 08:12:24 +0000

    tweak logic for plot-range-label

--- web/viewer.php
+++ web/viewer.php
@@ -404,7 +404,7 @@ The sites of the <a href="https://www.scec.org/research/cpd">SCEC Community Pale
            <span class="max"></span>
         </div>
     </div>
-    <div id="plot-range-label" class="row" style="opacity:0.8"> Label </div>
+    <div id="plot-range-label" class="row" style="display:;opacity:0.8"> Label </div>
 </div>
 
 <!-- modal list -->
