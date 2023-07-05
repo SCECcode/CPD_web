efa4f8cb24f7a6afe88efe2f7c892cae8fc17677
Mei-Hui Su <mei@MeiPro.local>
2023-07-05 09:26:07 -0700

    local changes

--- web/js/cpd_ui.js
+++ web/js/cpd_ui.js
@@ -21,13 +21,14 @@ window.console.log("calling showing key");
     $("#CPD_plot").prepend($("#plot-range-key-container").html());
     $("#plot-range-key span.min").html(min);
     $("#plot-range-key span.max").html(max);
+    $('#plot-range-label').css("display", "");
 }
 
 function removeKey() {
 window.console.log("calling removing key");
     if(showing_key) {
       $("#CPD_plot #plot-range-key").remove();
-      $("#CPD_plot #plot-range-label").remove();
+      $('#plot-range-label').css("display", "none");
       showing_key = false;
     }
 }
